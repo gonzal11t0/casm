09db61b807a97973e86b2ee809863b30a4954199
Gonzalo Lazarte <gonzalaz@live.com.ar>
2023-04-05 11:35:23 -0300

    settings

--- javaScript/index.js
+++ javaScript/index.js
@@ -12,7 +12,7 @@ hamburguerMenu.addEventListener("click", () => {
     if (link.style.animation) {
 		link.style.animation = "";
     } else {
-		link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+		link.style.animation = `navLinkFade 0.2s ease forwards ${index / 7 + 0.2}s`;
     }
 });
 hamburguerMenu.classList.toggle("toggle");
